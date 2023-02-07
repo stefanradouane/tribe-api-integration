dc22614e99a3b6307824b487cc1f136b84f1f7f9
your_username <your_email@gmail.com>
2023-02-07 19:58:04 +0100

    :bug: no query

--- netlify/functions/getUser.js
+++ netlify/functions/getUser.js
@@ -57,6 +57,9 @@ export const handler = async (event) => {
     Object.keys(queryObject).forEach(key => {
         const foundItem = finder.find(item => item == key)
         if(!foundItem) {
+            if(!queryObject){
+                return
+            }
             errorMessage = { error: 400, reason: "The query type is not supported", fix: "Try a query like: name, surname, nickname" };
             
         } else if (foundItem) {
