bc292f0fe62f41ffe2d499ffada7353e84cf89a8
Robert Spier <robert.spier@gmail.com>
2023-02-07 10:26:06 +0100

    Update hello-world.js

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,6 +1,12 @@
 export const handler = async () => {
     return {
       statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
       body: JSON.stringify({
         message: 'Hello World!',
       }),
