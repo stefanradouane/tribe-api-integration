9e89f5ef411b75cd07be8d818746e542925c48a8
your_username <your_email@gmail.com>
2023-02-07 12:48:13 +0100

    :sparkles: ADD getUser function

---
+++ netlify/functions/getUser.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+
+export const handler = async (event) => {
+    const name = "";
+    const surName = "";
+
+    let res = await fetch(`https://whois.fdnd.nl/api/v1/members?${event.rawQuery}`)
+    let data = await res.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        data: event
+      })
+    }
+  }
