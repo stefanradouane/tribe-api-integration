edb43ecf5b3928c09ae5baba336f989cb9d2b038
your_username <your_email@gmail.com>
2023-02-07 12:28:48 +0100

    :sparkles: ADD serverless functions

---
+++ netlify/functions/tribes.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+export const handler = async (event) => {
+    let res = await fetch(`https://whois.fdnd.nl/api/v1/tribes`)
+    let data = await res.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        data: data
+      })
+    }
+  }
