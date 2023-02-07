178396ead38c5bbd2505067e25a416307b21bdd7
Robert Spier <robert.spier@gmail.com>
2023-02-07 10:40:13 +0100

    test

---
+++ netlify/functions/members.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+export const handler = async () => {
+
+    let res = await fetch('https://whois.fdnd.nl/api/v1/members')
+    let data = await res.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
+      body: data
+    }
+  }
