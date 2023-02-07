d5ec2baf4d58e552f9a9cac1b47674c4cf919bd0
Robert Spier <robert.spier@gmail.com>
2023-02-07 10:53:37 +0100

    stringify

--- netlify/functions/members.js
+++ netlify/functions/members.js
@@ -13,8 +13,8 @@ export const handler = async () => {
         /* Required for cookies, authorization headers with HTTPS */
         'Access-Control-Allow-Credentials': true
       },
-      body: {
-        data: 'Test'
-      }
+      body: JSON.stringify({
+        data: data
+      })
     }
   }
